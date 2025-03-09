2f864be36f651dbc4be246597dbeb8245160cfda
p.k.m.p.perera <p.k.m.p.perera@gmail.com>
2025-03-09 20:50:14 +0530

    connectios

---
+++ Backend/routes/initialTestRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import DB from "../connection.js";
+import { ObjectId } from "mongodb";
+
+let router = express.Router();
+
+//read data
+router.route("/read").get(async (req, res) => {
+  let db = DB.getDB();
+  let result = await db.collection("initialTest").find({}).toArray();
+  res.json(result);
+});
+
+
+export default router;
