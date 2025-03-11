db3d10edd2324c00686839ccf4ca5e67093a8824
Dilum Randira <dilumrandira16@gmail.com>
2025-03-11 13:25:09 +0530

    test Quality implementation
    jnnsq

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/home";
 import Shop from "./pages/shop";
+import Quality from "./pages/quaity";
 
 //layouts
 import Layout from "./components/layouts/Layout";
@@ -15,6 +16,7 @@ function App() {
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
+            <Route path="/quality" element={<Quality />} />
           </Route>
         </Routes>
       </HashRouter>
---
+++ frontend/src/pages/quaity.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const quaity = () => {
+  return (
+    <>
+      <div>quaity</div>
+      
+      <div className="bg-emerald-600 w-[200px] md:bg-amber-600">yfhhjfghfh</div>
+    </>
+  );
+};
+
+export default quaity;
