30a0137f8fba4216bd0e353fcf2deef3a6039dfa
p.k.m.p.perera <p.k.m.p.perera@gmail.com>
2025-03-14 11:56:41 +0530

    navbar update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/home";
 import Shop from "./pages/shop";
-import Quality from "./pages/quaity";
 import Elements from "./pages/elements";
 import Access from "./pages/access";
 
@@ -23,8 +22,6 @@ function App() {
               {/* http://localhost:5173/*/}
               <Route path="/shop" element={<Shop />} />
               {/* http://localhost:5173/#/shop */}
-              <Route path="/quality" element={<Quality />} />
-              {/* http://localhost:5173/#/quality */}
               <Route path="/elements" element={<Elements />} />
               {/* http://localhost:5173/#/elements */}
             </Route>
