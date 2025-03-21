5647d4c6fe21b2403f6603437fd6ec6160bcf283
ThiliniAbeykoon1 <thiliniabekoon67@gmail.com>
2025-03-21 17:39:39 +0530

    implements to sales

--- frontend/src/pages/MainFunctions/Sales/subpages/sales_Manage_Order.tsx
+++ frontend/src/pages/MainFunctions/Sales/subpages/sales_Manage_Order.tsx
@@ -4,6 +4,7 @@ import Sales_Customer_table from "@/components/MainFunctions/sales/Sales_Manage_
 import Sales_Order_table from "@/components/MainFunctions/sales/Sales_Manage_Products/Sales_Order_table"
 import Sales_Payment_table from "@/components/MainFunctions/sales/Sales_Manage_Products/Sales_Payment_table"
 import Sales_Sales_table from "@/components/MainFunctions/sales/Sales_Manage_Products/Sales_Sales_table"
+import Sales_Order_Insert from "@/components/MainFunctions/sales/insert/Sales_Order_Insert"
 
 const sales_Manage_Order = () => {
   return (
@@ -11,6 +12,7 @@ const sales_Manage_Order = () => {
     s
     <div> order </div>
     <Sales_Order_table/>
+    <Sales_Order_Insert/>
     
     </>
   )
